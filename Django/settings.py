5b815e735c162f5bba3d9a98f0c7cd78a78cef4d
2194 <2194150786@qq.com>
2021-11-21 17:26:38 +0800

    add

--- Django/settings.py
+++ Django/settings.py
@@ -114,6 +114,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR / "static",)
+STATIC_ROOT = (BASE_DIR / "../static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
